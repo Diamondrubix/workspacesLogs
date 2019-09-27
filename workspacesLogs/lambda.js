7fd0e254eb45181cef9721736cd27ac6a21e4a35
Diamondrubix <Diamondrubix@gmail.com>
2019-09-27 15:23:48 -0500

    uyi

--- workspacesLogs/lambda.js
+++ workspacesLogs/lambda.js
@@ -12,16 +12,19 @@ var params = {
     'ws-5n920kbkv'
   ]
 };
+
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+
 workspaces.describeWorkspacesConnectionStatus(params, function(err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 });
   
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
+
     return response;
     
     
